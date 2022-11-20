6418ddb8b2ca999785789490f6fac645d2606467
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-11-21 00:33:06 +0300

    added new entity

--- src/IhaleProject.EntityFrameworkCore/EntityFrameworkCore/IhaleProjectDbContext.cs
+++ src/IhaleProject.EntityFrameworkCore/EntityFrameworkCore/IhaleProjectDbContext.cs
@@ -3,13 +3,15 @@ using Abp.Zero.EntityFrameworkCore;
 using IhaleProject.Authorization.Roles;
 using IhaleProject.Authorization.Users;
 using IhaleProject.MultiTenancy;
+using IhaleProject.Domain.Birim;
 
 namespace IhaleProject.EntityFrameworkCore
 {
     public class IhaleProjectDbContext : AbpZeroDbContext<Tenant, Role, User, IhaleProjectDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<BirimEntity> Birim { get; set; }
+
         public IhaleProjectDbContext(DbContextOptions<IhaleProjectDbContext> options)
             : base(options)
         {
