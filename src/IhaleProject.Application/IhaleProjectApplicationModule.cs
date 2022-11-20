64bbe92e8576a20823802d9bfe4c6087b32d3cc7
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-11-21 00:32:53 +0300

    added mapper and AbpFluentValidationModule

--- src/IhaleProject.Application/IhaleProjectApplicationModule.cs
+++ src/IhaleProject.Application/IhaleProjectApplicationModule.cs
@@ -1,13 +1,19 @@
 ﻿using Abp.AutoMapper;
+using Abp.FluentValidation;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
+using AutoMapper;
+using IhaleProject.Application.Contracts.Birim;
 using IhaleProject.Authorization;
+using IhaleProject.Domain.Birim;
 
 namespace IhaleProject
 {
     [DependsOn(
         typeof(IhaleProjectCoreModule), 
-        typeof(AbpAutoMapperModule))]
+        typeof(AbpAutoMapperModule),
+        typeof(AbpFluentValidationModule))
+        ]
     public class IhaleProjectApplicationModule : AbpModule
     {
         public override void PreInitialize()
@@ -18,12 +24,14 @@ namespace IhaleProject
         public override void Initialize()
         {
             var thisAssembly = typeof(IhaleProjectApplicationModule).GetAssembly();
-
+            var mapperAssembly = typeof(IhaleProjectApplicationAutoMapperProfile).GetAssembly();
+            
             IocManager.RegisterAssemblyByConvention(thisAssembly);
 
             Configuration.Modules.AbpAutoMapper().Configurators.Add(
                 // Scan the assembly for classes which inherit from AutoMapper.Profile
-                cfg => cfg.AddMaps(thisAssembly)
+                cfg => cfg.AddMaps(thisAssembly,mapperAssembly)
+                //cfg=>cfg.AddProfile<IhaleProjectApplicationAutoMapperProfile>()
             );
         }
     }
