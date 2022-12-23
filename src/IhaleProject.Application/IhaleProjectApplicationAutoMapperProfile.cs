e66099793d5e3cc7615411e2423f1373b7a56883
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-12-23 12:34:27 +0300

    added new maps

--- src/IhaleProject.Application/IhaleProjectApplicationAutoMapperProfile.cs
+++ src/IhaleProject.Application/IhaleProjectApplicationAutoMapperProfile.cs
@@ -1,5 +1,9 @@
 ﻿using AutoMapper;
+using IhaleProject.Application.Contracts.AlimTuru;
+using IhaleProject.Application.Contracts.AlimUsulu;
 using IhaleProject.Application.Contracts.Birim;
+using IhaleProject.Domain.AlimTuru;
+using IhaleProject.Domain.AlimUsulu;
 using IhaleProject.Domain.Birim;
 
 namespace IhaleProject
@@ -12,6 +16,16 @@ namespace IhaleProject
             CreateMap<BirimEntity, BirimDto>();
             CreateMap<CreateBirimDto, BirimEntity>();
             CreateMap<UpdateBirimDto, BirimEntity>();
+
+            //AlimTuru
+            CreateMap<AlimTuruEntity, AlimTuruDTO>();
+            CreateMap<CreateAlimTuruDTO, AlimTuruEntity>();
+            CreateMap<UpdateAlimTuruDTO, AlimTuruEntity>();
+
+            //AlimUsulu
+            CreateMap<AlimUsuluEntity, AlimUsuluDTO>();
+            CreateMap<CreateAlimUsuluDTO, AlimUsuluEntity>();
+            CreateMap<UpdateAlimUsuluDTO, AlimUsuluEntity>();
         }
     }
 }
