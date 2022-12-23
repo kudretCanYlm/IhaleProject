093b3508a4305567db843a587c6dfb34dd8e89f1
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-12-23 12:34:42 +0300

    refactored

--- src/IhaleProject.Application/IhaleProjectApplicationModule.cs
+++ src/IhaleProject.Application/IhaleProjectApplicationModule.cs
@@ -24,13 +24,13 @@ namespace IhaleProject
         public override void Initialize()
         {
             var thisAssembly = typeof(IhaleProjectApplicationModule).GetAssembly();
-            var mapperAssembly = typeof(IhaleProjectApplicationAutoMapperProfile).GetAssembly();
+            //var mapperAssembly = typeof(IhaleProjectApplicationAutoMapperProfile).GetAssembly();
             
             IocManager.RegisterAssemblyByConvention(thisAssembly);
 
             Configuration.Modules.AbpAutoMapper().Configurators.Add(
                 // Scan the assembly for classes which inherit from AutoMapper.Profile
-                cfg => cfg.AddMaps(thisAssembly,mapperAssembly)
+                cfg => cfg.AddMaps(thisAssembly)
                 //cfg=>cfg.AddProfile<IhaleProjectApplicationAutoMapperProfile>()
             );
         }
