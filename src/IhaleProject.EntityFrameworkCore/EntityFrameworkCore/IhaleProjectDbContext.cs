69fb2fbb58d6b10a1a5ba4811c0d9d5149ae098a
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-12-23 12:35:17 +0300

    added new tables

--- src/IhaleProject.EntityFrameworkCore/EntityFrameworkCore/IhaleProjectDbContext.cs
+++ src/IhaleProject.EntityFrameworkCore/EntityFrameworkCore/IhaleProjectDbContext.cs
@@ -4,13 +4,22 @@ using IhaleProject.Authorization.Roles;
 using IhaleProject.Authorization.Users;
 using IhaleProject.MultiTenancy;
 using IhaleProject.Domain.Birim;
+using IhaleProject.Domain.AlimTuru;
+using IhaleProject.Domain.AlimUsulu;
 
 namespace IhaleProject.EntityFrameworkCore
 {
     public class IhaleProjectDbContext : AbpZeroDbContext<Tenant, Role, User, IhaleProjectDbContext>
     {
-        /* Define a DbSet for each entity of the application */
-        public DbSet<BirimEntity> Birim { get; set; }
+		/* Define a DbSet for each entity of the application */
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+            //will add table names
+			base.OnModelCreating(modelBuilder);
+		}
+		public DbSet<BirimEntity> Birim { get; set; }
+        public DbSet<AlimTuruEntity> AlimTuru { get; set; }
+        public DbSet<AlimUsuluEntity> AlimUsulu { get; set; }
 
         public IhaleProjectDbContext(DbContextOptions<IhaleProjectDbContext> options)
             : base(options)
