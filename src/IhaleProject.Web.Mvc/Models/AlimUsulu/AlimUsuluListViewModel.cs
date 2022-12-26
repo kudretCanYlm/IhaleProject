dc5fb089ae84b3be745078019536924b9bf72393
Rafet Karaca <karacarafet32@gmail.com>
2022-12-26 16:24:45 +0300

    ViewModels

---
+++ src/IhaleProject.Web.Mvc/Models/AlimUsulu/AlimUsuluListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using IhaleProject.Application.Contracts.AlimTuru;
+using IhaleProject.Application.Contracts.AlimUsulu;
+using System.Collections.Generic;
+
+namespace IhaleProject.Web.Models.AlimUsulu
+{
+	public class AlimUsuluListViewModel
+	{
+		public IEnumerable<AlimUsuluDTO> alimUsulleri { get; set; }
+	}
+}
--- src/IhaleProject.Web.Mvc/Models/Birim/BirimListViewModel.cs
+++ src/IhaleProject.Web.Mvc/Models/Birim/BirimListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using IhaleProject.Application.Contracts.Birim;
+﻿using IhaleProject.Application.Contracts.AlimUsulu;
+using IhaleProject.Application.Contracts.Birim;
 using System.Collections.Generic;
 
 namespace IhaleProject.Web.Models.Birim
@@ -6,5 +7,6 @@ namespace IhaleProject.Web.Models.Birim
     public class BirimListViewModel
     {
         public IEnumerable<BirimDto> Birims { get; set; }
-    }
+		public IEnumerable<AlimUsuluDTO> alimUsullleri { get; internal set; }
+	}
 }
