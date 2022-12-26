4869ebc5c0e1437db56f1395de9eb0474217e2dc
Rafet Karaca <karacarafet32@gmail.com>
2022-12-26 16:24:25 +0300

    new func added

--- src/IhaleProject.Web.Mvc/Controllers/AlimTuruController.cs
+++ src/IhaleProject.Web.Mvc/Controllers/AlimTuruController.cs
@@ -1,8 +1,15 @@
 ﻿using Abp.AspNetCore.Mvc.Authorization;
+using FluentValidation;
+using FluentValidation.Results;
 using IhaleProject.Application.Contracts.AlimTuru;
+using IhaleProject.Application.Contracts.Birim;
 using IhaleProject.Authorization;
+using IhaleProject.Birim;
 using IhaleProject.Controllers;
+using IhaleProject.Web.Models.AlimTuru;
+using IhaleProject.Web.Models.Birim;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace IhaleProject.Web.Controllers
@@ -23,5 +30,86 @@ namespace IhaleProject.Web.Controllers
 		{
 			return View();
 		}
+
+		[HttpGet]
+		public async Task<JsonResult> GetAlimTurleri()
+		{
+			var AlimTurleri = await alimTuruAppService.GetAllAsync();
+
+			return Json(new AlimTuruListViewModel()
+			{
+				alimTurleri = AlimTurleri
+			});
+		}
+
+		[HttpGet]
+		public async Task<JsonResult> GetAlimTuru(Guid id)
+		{
+			var AlimTuru = await alimTuruAppService.GetAsync(id);
+
+			return Json(AlimTuru);
+		}
+
+		[HttpPost]
+		public async Task<JsonResult> Delete(Guid Id)
+		{
+			await alimTuruAppService.DeleteAsync(Id);
+
+			return Json(new { isSuccess = true });
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(CreateAlimTuruDTO createAlimTuruDTO)
+		{
+			//add vaidations
+			var result = createAlimTuruValidator.Validate(createAlimTuruDTO);
+
+			if (result.IsValid)
+			{
+				await alimTuruAppService.CreateAsync(createAlimTuruDTO);
+			}
+
+			else
+			{
+				foreach (ValidationFailure failer in result.Errors)
+				{
+
+					ModelState.AddModelError(failer.PropertyName, failer.ErrorMessage);
+
+				}
+
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public async Task<JsonResult> Update([FromRoute] Guid id, UpdateAlimTuruDTO updateAlimTuruDTO)
+		{
+
+			//add vaidations
+			var result = updateAlimTuruValidator.Validate(updateAlimTuruDTO);
+
+			if (result.IsValid)
+			{
+				await alimTuruAppService.UpdateAsync(id, updateAlimTuruDTO);
+			}
+
+			else
+			{
+				foreach (ValidationFailure failer in result.Errors)
+				{
+
+					ModelState.AddModelError(failer.PropertyName, failer.ErrorMessage);
+
+					return Json(new { ModelState });
+				}
+
+			}
+
+			return Json(new { ModelState });
+
+		}
+
 	}
 }
