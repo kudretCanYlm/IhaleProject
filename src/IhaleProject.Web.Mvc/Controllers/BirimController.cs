44bc0dbbe700b3add095971660d7c1964bc43ae8
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-12-26 11:03:21 +0300

    added new functions

--- src/IhaleProject.Web.Mvc/Controllers/BirimController.cs
+++ src/IhaleProject.Web.Mvc/Controllers/BirimController.cs
@@ -9,56 +9,82 @@ using System.Threading.Tasks;
 
 namespace IhaleProject.Web.Controllers
 {
-    //[AbpMvcAuthorize(PermissionNames.Pages_Users)]
-    public class BirimController : IhaleProjectControllerBase
-    {
-        private readonly IBirimAppService birimAppService;
-        //will access with ICreateBirimDtoValidator interface
-        private readonly CreateBirimDtoValidator createBirimValidator = new CreateBirimDtoValidator();
-        private readonly UpdateBirimDtoValidator updateBirimValidator = new UpdateBirimDtoValidator();
+	//[AbpMvcAuthorize(PermissionNames.Pages_Users)]
+	public class BirimController : IhaleProjectControllerBase
+	{
+		private readonly IBirimAppService birimAppService;
+		//will access with ICreateBirimDtoValidator interface
+		private readonly CreateBirimDtoValidator createBirimValidator = new CreateBirimDtoValidator();
+		private readonly UpdateBirimDtoValidator updateBirimValidator = new UpdateBirimDtoValidator();
 
-        public BirimController(IBirimAppService birimAppService)
-        {
-            this.birimAppService = birimAppService;
-        }
+		public BirimController(IBirimAppService birimAppService)
+		{
+			this.birimAppService = birimAppService;
+		}
 
-        public  IActionResult Index()
-        {
-            //will add short fluent validation control
+		public IActionResult Index()
+		{
+			//will add short fluent validation control
 
-            return View();
+			return View();
 
-        }
+		}
 
-        [HttpGet]
-        //[WrapResult(wrapOnSuccess:false)]
-        public async Task<JsonResult> GetBirimler()
-        {
-            var birimler = await birimAppService.GetAllAsync();
+		[HttpGet]
+		//[WrapResult(wrapOnSuccess:false)]
+		public async Task<JsonResult> GetBirimler()
+		{
+			var birimler = await birimAppService.GetAllAsync();
 
-            return Json(new BirimListViewModel()
-            {
-                Birims = birimler
-            });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(Guid Id)
-        {
-            await birimAppService.DeleteAsync(Id);
-
-            return RedirectToAction(L(nameof(Index)));
-        }
+			return Json(new BirimListViewModel()
+			{
+				Birims = birimler
+			});
+		}
 
 		[HttpPost]
-        public async Task<IActionResult> Create(CreateBirimDto createBirimDto)
-        {
+		public async Task<JsonResult> Delete(Guid Id)
+		{
+			await birimAppService.DeleteAsync(Id);
+
+			return Json(new { isSuccess = true });
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(CreateBirimDto createBirimDto)
+		{
+			//add vaidations
+			var result = createBirimValidator.Validate(createBirimDto);
+
+			if (result.IsValid)
+			{
+				await birimAppService.CreateAsync(createBirimDto);
+			}
+
+			else
+			{
+				foreach (ValidationFailure failer in result.Errors)
+				{
+
+					ModelState.AddModelError(failer.PropertyName, failer.ErrorMessage);
+
+				}
+
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Update([FromQuery]Guid id,[FromBody]UpdateBirimDto updateBirimDto)
+		{
+
             //add vaidations
-            var result = createBirimValidator.Validate(createBirimDto);
+            var result = updateBirimValidator.Validate(updateBirimDto);
 
             if (result.IsValid)
             {
-                await birimAppService.CreateAsync(createBirimDto);
+                await birimAppService.UpdateAsync(id, updateBirimDto);
             }
 
             else
@@ -75,5 +101,5 @@ namespace IhaleProject.Web.Controllers
             return RedirectToAction("Index");
 
         }
-    }
+	}
 }
