c009d7ecf41dfb90f5c56126b76f840a2e1bdb1c
Rafet Karaca <karacarafet32@gmail.com>
2023-01-02 17:03:37 +0300

    Mail

--- IhaleProject.Domain/EmailSender/TaskManager.cs
+++ IhaleProject.Domain/EmailSender/TaskManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using Abp.Domain.Services;
 using Abp.Net.Mail;
+using System.Net;
+using System.Net.Mail;
 
 namespace IhaleProject.Domain.EmailSender
 {
@@ -17,18 +20,24 @@ namespace IhaleProject.Domain.EmailSender
 				_emailSender = emailSender;
 			}
 
+			//var fromAddress = new MailAddress("sender@example.com", "Sender");
+			//var toAddress = new MailAddress("receiver@example.com", "Receiver");
+			//const string subject = "My Mail Subject";
+			//const string body = "My Mail";
+
+
 			//public void Assign(Task task, Person person)
 			//{
 			//	//Assign task to the person
 			//	task.AssignedTo = person;
 
-			//	//Send a notification email
-			//	_emailSender.Send(
-			//		to: person.EmailAddress,
-			//		subject: "You have a new task!",
-			//		body: $"A new task is assigned for you: <b>{task.Title}</b>",
-			//		isBodyHtml: true
-			//	);
-			//}
+	//	//Send a notification email
+	//	_emailSender.Send(
+	//		to: person.EmailAddress,
+	//		subject: "You have a new task!",
+	//		body: $"A new task is assigned for you: <b>{task.Title}</b>",
+	//		isBodyHtml: true
+	//	);
+	//}
 	}
 }
