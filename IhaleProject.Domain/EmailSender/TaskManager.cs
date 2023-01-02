e0d58b45cc1fc0c6e1047c8c22a83aa8bd72d92f
Rafet Karaca <karacarafet32@gmail.com>
2023-01-02 17:14:53 +0300

    MailAt

--- IhaleProject.Domain/EmailSender/TaskManager.cs
+++ IhaleProject.Domain/EmailSender/TaskManager.cs
@@ -19,25 +19,26 @@ namespace IhaleProject.Domain.EmailSender
 			{
 				_emailSender = emailSender;
 			}
+		void MailAt()
+		{
+			var fromAddress = new MailAddress("sender@example.com", "Sender");
+			var toAddress = new MailAddress("receiver@example.com", "Receiver");
+			const string subject = "My Mail Subject";
+			const string body = "My Mail";
+		}
 
-			//var fromAddress = new MailAddress("sender@example.com", "Sender");
-			//var toAddress = new MailAddress("receiver@example.com", "Receiver");
-			//const string subject = "My Mail Subject";
-			//const string body = "My Mail";
+		//public void Assign(Task task, Person person)
+		//{
+		//	//Assign task to the person
+		//	task.AssignedTo = person;
 
-
-			//public void Assign(Task task, Person person)
-			//{
-			//	//Assign task to the person
-			//	task.AssignedTo = person;
-
-	//	//Send a notification email
-	//	_emailSender.Send(
-	//		to: person.EmailAddress,
-	//		subject: "You have a new task!",
-	//		body: $"A new task is assigned for you: <b>{task.Title}</b>",
-	//		isBodyHtml: true
-	//	);
-	//}
+		//	//Send a notification email
+		//	_emailSender.Send(
+		//		to: person.EmailAddress,
+		//		subject: "You have a new task!",
+		//		body: $"A new task is assigned for you: <b>{task.Title}</b>",
+		//		isBodyHtml: true
+		//	);
+		//}
 	}
 }
