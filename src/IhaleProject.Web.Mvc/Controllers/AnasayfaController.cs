a1ba9e9a0a412ed114406df65118ba2c454f7dd0
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-01-06 13:13:35 +0300

    added

--- src/IhaleProject.Web.Mvc/Controllers/AnasayfaController.cs
+++ src/IhaleProject.Web.Mvc/Controllers/AnasayfaController.cs
@@ -1,13 +1,47 @@
 ﻿using IhaleProject.Controllers;
+using IhaleProject.Ihale;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using IhaleProject.Application.Contracts.Email;
 
 namespace IhaleProject.Web.Controllers
 {
 	public class AnasayfaController : IhaleProjectControllerBase
 	{
+
+		private readonly IEmailAppService emailAppService;
+
+		public AnasayfaController(IEmailAppService emailAppService)
+		{
+			this.emailAppService = emailAppService;
+		}
+
 		public IActionResult Listele()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> MailEkle(CreateEmailDto mail)
+		{
+			try
+			{
+				bool status=await emailAppService.IsEmailAlreadyUsed(mail);
+
+				if(status)
+					return BadRequest();
+
+				await emailAppService.Create(mail);
+
+				return Ok();
+				
+			}
+			catch (Exception)
+			{
+
+				return BadRequest();
+			}
 		}
 	}
 }
