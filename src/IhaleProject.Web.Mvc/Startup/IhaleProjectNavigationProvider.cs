817056a4160ed9f9e5b1e494bf1f51867d6d1295
Rafet Karaca <karacarafet32@gmail.com>
2023-01-06 14:51:31 +0300

    View Tarafı Değişiklikler

--- src/IhaleProject.Web.Mvc/Startup/IhaleProjectNavigationProvider.cs
+++ src/IhaleProject.Web.Mvc/Startup/IhaleProjectNavigationProvider.cs
@@ -16,86 +16,24 @@ namespace IhaleProject.Web.Startup
 		{
 			context.Manager.MainMenu
 				.AddItem(
-					new MenuItemDefinition(
-						PageNames.About,
-						L("About"),
-						url: "About",
-						icon: "fas fa-info-circle"
-					)
-				)
-				.AddItem(
-					new MenuItemDefinition(
-						PageNames.Home,
-						L("HomePage"),
-						url: "",
-						icon: "fas fa-home",
-						requiresAuthentication: true
-					)
-				).AddItem(
-					new MenuItemDefinition(
-						PageNames.Tenants,
-						L("Tenants"),
-						url: "Tenants",
-						icon: "fas fa-building",
-						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Tenants)
-					)
-				).AddItem(
-					new MenuItemDefinition(
-						PageNames.Users,
-						L("Users"),
-						url: "Users",
-						icon: "fas fa-users",
-						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Users)
-					)
-				).AddItem(new MenuItemDefinition(
-					"Parametreler",
-					L("Parametreler"),
-					icon: "fas fa-circle"
-					).AddItem(
-					new MenuItemDefinition(
-						PageNames.Birim,
-						L("Birim"),
-						url: "Birim",
-						icon: "fa fa-cubes",
-						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Birim)
-					)
-					).AddItem(
-					new MenuItemDefinition(
-						PageNames.AlimTuru,
-						L("AlimTuru"),
-						url: "AlimTuru",
-						icon: "fa fa-cubes",
-						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_AlimTuru)
-					)
-					).AddItem(
-					new MenuItemDefinition(
-						PageNames.AlimUsulu,
-						L("AlimUsulu"),
-						url: "AlimUsulu",
-						icon: "fa fa-cubes",
-						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_AlimUsulu)
-					)
-				))
-
-				.AddItem(
 				new MenuItemDefinition(
 					"İhale İşlemleri",
 					L("IhaleIslemleri"),
-					icon: "fas fa-circle"
+					icon: "fas fa-building"
 					).AddItem(
 				new MenuItemDefinition(
 					PageNames.Ihale,
 					L("Ihale"),
-					url: "Ihale",
-					icon: "fa fa-cubes",
+					url: "Ihale/Index",
+					icon: "fa fa-square",
 					permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Ihale)
 					)
 				).AddItem(
 				new MenuItemDefinition(
 					PageNames.Arsiv,
 					L("Arsiv"),
-					url: "Arsiv",
-					icon: "fa fa-cubes",
+					url: "Arsiv/Index",
+					icon: "fa fa-square",
 					permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Arsiv)
 					)
 				)
@@ -103,8 +41,8 @@ namespace IhaleProject.Web.Startup
 				new MenuItemDefinition(
 					PageNames.TumYayinlar,
 					L("TumYayinlar"),
-					url: "TumYayinlar",
-					icon: "fa fa-cubes",
+					url: "TumYayinlar/Index",
+					icon: "fa fa-square",
 					permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_TumYayinlar)
 					)
 				)
@@ -112,17 +50,47 @@ namespace IhaleProject.Web.Startup
 				new MenuItemDefinition(
 					PageNames.KaldirilmisYayinlar,
 					L("KaldirilmisYayinlar"),
-					url: "KaldirilmisYayinlar",
-					icon: "fa fa-cubes",
+					url: "KaldirilmisYayinlar/Index",
+					icon: "fa fa-square",
 					permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_KaldirilmisYayinlar)
 					)
 				).AddItem(
 				new MenuItemDefinition(
 					PageNames.SuresiBitmisYayinlar,
 					L("SuresiBitmisYayinlar"),
-					url: "SuresiBitmisYayinlar",
-					icon: "fa fa-cubes",
+					url: "SuresiBitmisYayinlar/Index",
+					icon: "fa fa-square",
 					permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_SuresiBitmisYayinlar)
+					)
+				)
+				)
+				.AddItem(new MenuItemDefinition(
+					"Parametreler",
+					L("Parametreler"),
+					icon: "fas fa-cubes"
+					).AddItem(
+					new MenuItemDefinition(
+						PageNames.Birim,
+						L("Birim"),
+						url: "Birim/Index",
+						icon: "fa fa-circle",
+						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Birim)
+					)
+					).AddItem(
+					new MenuItemDefinition(
+						PageNames.AlimTuru,
+						L("AlimTuru"),
+						url: "AlimTuru/Index",
+						icon: "fa fa-circle",
+						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_AlimTuru)
+					)
+					).AddItem(
+					new MenuItemDefinition(
+						PageNames.AlimUsulu,
+						L("AlimUsulu"),
+						url: "AlimUsulu/Index",
+						icon: "fa fa-circle",
+						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_AlimUsulu)
 					)
 				)
 				)
@@ -131,93 +99,33 @@ namespace IhaleProject.Web.Startup
 					new MenuItemDefinition(
 						PageNames.Roles,
 						L("Roles"),
-						url: "Roles",
+						url: "Roles/Index",
 						icon: "fas fa-theater-masks",
 						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Roles)
 					)
 				)
-				.AddItem( // Menu items below is just for demonstration!
+
+
+				.AddItem(
 					new MenuItemDefinition(
-						"MultiLevelMenu",
-						L("MultiLevelMenu"),
-						icon: "fas fa-circle"
-					).AddItem(
-						new MenuItemDefinition(
-							"AspNetBoilerplate",
-							new FixedLocalizableString("ASP.NET Boilerplate"),
-							icon: "far fa-circle"
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetBoilerplateHome",
-								new FixedLocalizableString("Home"),
-								url: "https://aspnetboilerplate.com?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetBoilerplateTemplates",
-								new FixedLocalizableString("Templates"),
-								url: "https://aspnetboilerplate.com/Templates?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetBoilerplateSamples",
-								new FixedLocalizableString("Samples"),
-								url: "https://aspnetboilerplate.com/Samples?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetBoilerplateDocuments",
-								new FixedLocalizableString("Documents"),
-								url: "https://aspnetboilerplate.com/Pages/Documents?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						)
-					).AddItem(
-						new MenuItemDefinition(
-							"AspNetZero",
-							new FixedLocalizableString("ASP.NET Zero"),
-							icon: "far fa-circle"
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetZeroHome",
-								new FixedLocalizableString("Home"),
-								url: "https://aspnetzero.com?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetZeroFeatures",
-								new FixedLocalizableString("Features"),
-								url: "https://aspnetzero.com/Features?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetZeroPricing",
-								new FixedLocalizableString("Pricing"),
-								url: "https://aspnetzero.com/Pricing?ref=abptmpl#pricing",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetZeroFaq",
-								new FixedLocalizableString("Faq"),
-								url: "https://aspnetzero.com/Faq?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						).AddItem(
-							new MenuItemDefinition(
-								"AspNetZeroDocuments",
-								new FixedLocalizableString("Documents"),
-								url: "https://aspnetzero.com/Documents?ref=abptmpl",
-								icon: "far fa-dot-circle"
-							)
-						)
+						PageNames.Users,
+						L("Users"),
+						url: "Users/Index",
+						icon: "fas fa-users",
+						permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Users)
 					)
-				);
+				)
+
+				//.AddItem(
+				//	new MenuItemDefinition(
+				//		PageNames.Tenants,
+				//		L("Tenants"),
+				//		url: "Tenants",
+				//		icon: "fas fa-building",
+				//		permissionDependency: new SimplePermissionDependency(PermissionNames.Pages_Tenants)
+				//	)
+				//)
+				;
 		}
 
 		private static ILocalizableString L(string name)
